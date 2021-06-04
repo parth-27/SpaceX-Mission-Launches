43cb39db5767f402aca9332bfd2bdeba60059d17
parth-27 <parth1020patel@gmail.com>
2021-06-04 18:28:39 +0530

    navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import space from './Spacex.png';
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar fixed-top navbar-expand-sm bg-dark">
+                <Link to="/">
+                    <img src={space} alt="" />
+                </Link>
+                <ul className="navbar-nav ml-auto" style={{ paddingRight: "5%",fontSize:"1.5rem" }}>
+                    {/* <li class="nav-item" style={{ paddingRight: "20%" }}>
+                        <Link to="missions/">
+                            Missions
+                        </Link>
+                    </li> */}
+                    <li className="nav-item">
+                        <Link to="launches/">
+                            Launches
+                        </Link>
+                    </li>
+                    
+                </ul>
+            </nav>
+        )
+    }
+}
