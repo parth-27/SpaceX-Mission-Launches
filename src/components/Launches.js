49a8bbb42dda38524e23bb7f6f3621de94e38db3
parth-27 <parth1020patel@gmail.com>
2021-06-04 18:28:00 +0530

    launches

---
+++ src/components/Launches.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import LaunchItem from './LaunchItem';
+
+const URL = "https://api.spacexdata.com/v4/launches/"
+
+export default class Launches extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoaded: false,
+            spaceData: []
+        };
+    }
+
+    componentDidMount() {
+        axios(`${URL}`)
+            .then(response => {
+                this.setState({
+                    spaceData: [...response.data],
+                    isLoaded: true
+                }, () => console.log("new", this.state));
+            }).catch((err) => console.error(err));
+    }
+
+    render() {
+        const launch = this.state.spaceData.map((launch) => {
+            return <LaunchItem key={launch.id} launch={launch} />   
+        })
+        
+        return (
+            <>
+                <h1 className="display-4 my-4">Launches</h1>
+                {launch}
+            </>
+        )
+    }
+}
